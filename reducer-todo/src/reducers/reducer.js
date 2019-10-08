c514f502f06fccae2d991a5a49962d807f8168ec
unknown <oluwatolablessing@gmail.com>
2019-10-08 14:52:09 +0100

    Rendering done

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,8 +1,19 @@
-export const initialState = {
-    item: 'Learn about reducers',
+export const initialState = [{
+    item: 'Wake up',
     completed: false,
-    id: 3892987589
-  }
+    id: 0
+},
+{  
+    item: 'Pray',
+    completed: false,
+    id: 1
+},
+{  
+    item: 'Eat',
+    completed: false,
+    id: 2
+},
+]
 
 export function reducer(state, action) {
     switch (action.type) {
